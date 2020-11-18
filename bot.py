aaffa849e25804a5b5eb2a1a76c703da3b09cbf1
clancy <73657402+clancyy@users.noreply.github.com>
2020-11-18 12:27:46 +0300

    Added custom activity.
    For more info: https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.Bot.change_presence

--- bot.py
+++ bot.py
@@ -9,8 +9,26 @@ client.remove_command("help") # This could be useful if you're planning on makin
 
 cogs = ['cogs.x', 'cogs.y', 'cogs.z'] # 'cogs' signifies the name of the folder, 'x' signifies the file name.
 
+# You can also change the bot's discord activity, its not necessary but if you want it maybe useful down the line.
+# Not that its mandatory but it maybe useful down the line.
+
+activity_status = "YOUR BOT ACTIVITY"
+
+# Types of Activities:
+
 @client.event
 async def on_ready():
+    # Types of Activities: Playing, Streaming, Listening, Watching.
+    
+    # > For more info: https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.Bot.change_presence
+    
+    # They're commented out, you can only choose one. You can't have 2 running at a time.
+    
+    # await client.change_presence(activity=discord.Game(name=activity_status))
+    # await client.change_presence(activity=discord.Streaming(name="My Stream", url=my_twitch_url))
+    # await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity_status))
+    # await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie"))
+    
     print(f'Logged in as: {client.user.name}')
     print(f'With ID: {client.user.id}')
 
