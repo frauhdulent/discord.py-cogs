ff434e2a2f814515d8f15f61f2733d85cd790c04
clancy <73657402+clancyy@users.noreply.github.com>
2020-11-21 02:23:30 +0300

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-# You can change 'x' to anything.
+# You can change 'Moderation' to anything.
 class Moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
