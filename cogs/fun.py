8d8405c1d61210baa03e90f4b3f5af5dfa64785c
clancy <73657402+clancyy@users.noreply.github.com>
2020-11-21 02:24:06 +0300

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-# You can change 'y' to anything.
+# You can change 'Fun' to anything.
 class Fun(commands.Cog):
     def __init__(self, client):
         self.client = client
