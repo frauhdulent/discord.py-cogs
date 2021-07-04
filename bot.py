bade22dc6ca3414bc79658317adbda57934ed03b
Nolan <73657402+yNolan@users.noreply.github.com>
2021-07-04 13:38:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,18 @@
 import discord
 from discord.ext import commands
 
-TOKEN = 'YOUR BOT TOKEN' # Variable for storing your token.
-PREFIX = ['BOT PREFIX'] # Variable for storing the bots prefix. eg:- !, ?, +, -
+TOKEN = 'YOUR BOT TOKEN'
+PREFIX = ['BOT PREFIX']
 
-client = commands.Bot(command_prefix = PREFIX) # Using the value stored in variable 'PREFIX'
-client.remove_command("help") # This could be useful if you're planning on making a custom help command down the line.
+client = commands.Bot(command_prefix = PREFIX)
+client.remove_command("help")
 
 cogs = ['cogs.fun', 'cogs.mod', 'cogs.misc'] # 'cogs' signifies the name of the folder, 'x' signifies the file name.
 
-# You can also change the bot's discord activity, its not necessary but if you want it maybe useful down the line.
-# Not that its mandatory but it maybe useful down the line.
-
 activity_status = "YOUR BOT ACTIVITY"
-
-# Types of Activities:
 
 @client.event
 async def on_ready():
-    # Types of Activities: Playing, Streaming, Listening, Watching.
-    
-    # > For more info: https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.Bot.change_presence
-    
-    # They're commented out, you can only choose one. You can't have 2 running at a time.
-    
-    # await client.change_presence(activity=discord.Game(name=activity_status))
-    # await client.change_presence(activity=discord.Streaming(name="My Stream", url=my_twitch_url))
-    # await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity_status))
-    # await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie"))
-    
     print(f'Logged in as: {client.user.name}')
     print(f'With ID: {client.user.id}')
 
@@ -36,4 +20,4 @@ async def on_ready():
         client.load_extension(cog) # Loads the cogs.
     return
 
-client.run(TOKEN) # Using the value stored in variable 'TOKEN'
+client.run(TOKEN)
